34b76c65ccccefc8fe7f55ede4be1b0f1f72ef49
fengweitao <fengweitao@sensetime.com>
2017-07-10 01:44:24 +0800

    test

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+void getPack(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * packet) {
+	int * id = (int * ) arg;
+	printf("id: %d\n", ++(*id));
+	printf("len: %d\n", pkthdr->len);
+	printf("num of bytes: %d\n", pkthdr->caplen);
+	printf("time: %s\n", ctime((const time_t *)&pkthdr->ts.tv_sec));
+
+	for (size_t i=0;i<pkthdr->len;++i) {
+		printf("%02x ", packet[i]);
+	}
+	puts("\n");
+}
+
 int main() {
     char buff[PCAP_ERRBUF_SIZE], *device;
 
@@ -11,12 +24,9 @@ int main() {
     if (device) {
         printf("device: %s\n", device);
         pcap_t * dev = pcap_open_live(device, 65535, 1, 0, buff);
-        struct pcap_pkthdr packet;
-        const u_char * pktStr = pcap_next(dev, &packet);
-        if (pktStr) {
-            cout<<packet.len<<endl;
-            pcap_close(dev);
-        }
+		int id = 0;
+		pcap_loop(dev, -1, getPack, (u_char*)&id);
+		pcap_close(dev);
     } else puts(buff);
     return 0;
 }
